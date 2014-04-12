d86915a7a2bea34808b90b03eb2368d0fe7e107d
Vincent Plagnol <vincent.plagnol@gmail.com>
2014-04-12 03:42:34 +0100

    Now a proper handling of the first exon of each chromosome, by runnig the Viterbi algorithm separately for each chromosome

---
+++ test/vignette_test.R
@@ -0,0 +1,56 @@
+library(GenomicRanges)
+library(VGAM)
+library(aod)
+
+library(ExomeDepth)
+source("R/class_definition.R")
+source("R/tools.R")
+source("R/optimize_reference_set.R")
+dyn.load('src/ExomeDepth.so')
+
+
+
+viterbi.hmm <- function(transitions, loglikelihood, positions) {
+    if ( nrow(transitions) != ncol(transitions) ) stop("Transition matrix is not square")
+      nstates <- nrow(transitions)
+      nobs <- nrow(loglikelihood)
+
+      res <- .Call("C_hmm", nstates, nobs, transitions, loglikelihood, positions)
+      dimnames(res[[2]])[[2]] <- c('start.p', 'end.p', 'type', 'nexons')
+      res[[2]] <- as.data.frame(res[[2]])
+      names(res) <- c('Viterbi.path', 'calls')
+
+      return(res)
+  }
+
+
+load('data/ExomeCount.RData')
+data <- as(ExomeCount, 'data.frame')
+data$space[1:10000] <- 2
+
+test <- new('ExomeDepth',
+            test = data$Exome2,
+            reference = data$Exome4,
+            formula = 'cbind(test, reference) ~ 1',
+            subset.for.speed = seq(1, nrow(data), 100))
+
+show(test)
+
+
+
+my.test <- data$Exome3
+my.reference.set <- as.matrix(data[, c('Exome1', 'Exome2', 'Exome4')])
+my.choice <- select.reference.set (test.counts = my.test, reference.counts = my.reference.set, bin.length = (data$end - data$start)/1000, n.bins.reduced = 10000)
+#print(my.choice)
+
+my.reference.selected <- apply(as.matrix( data[, my.choice$reference.choice] ), MAR = 1, FUN = sum)
+
+
+test <- new('ExomeDepth',
+            test = my.test,
+            reference = my.reference.selected,
+            formula = 'cbind(test, reference) ~ 1')
+
+#source('R/call_CNVs_method.R')
+my.calls <- CallCNVs(test, transition.probability = 10^-4, chromosome = data$space, start = data$start, end = data$end, name = data$names)
+print(my.calls@CNV.calls)
